efa6cc888b35b48ab74ad289cd9c72475b1773f6
Jeff Liao <zjliao@ucsd.edu>
2020-02-07 13:35:14 -0800

    stuff

--- class_hw/w4_apiGraphs/collection.py
+++ class_hw/w4_apiGraphs/collection.py
@@ -1,5 +1,9 @@
 from glob import glob
 
 
-def get_smali(smali_path, **kwargs):
+def get_smali(smali_path):
+    '''
+    get all filenames in smali_path with .smali
+    input: directory
+    '''
     return glob('%s/**/*.smali' % smali_path, recursive=True)
